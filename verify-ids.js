398073acc2186e54294a71dafb0d4003c7ed9782
Jakob Fix <jakob.fix@gmail.com>
2022-04-05 23:32:03 +0200

    Get correct results

--- verify-ids.js
+++ verify-ids.js
@@ -2,52 +2,33 @@ const fs = require('fs');
 const { parse } = require('csv-parse');
 const { stringify } = require('csv-stringify');
 
-// const sl = fs.readFileSync('./statlinkids.txt').toString().replace(/\r\n/g, '\n').split('\n');
+const inFileName = "DOI_Mismatch.csv"
+const regexId = /([0-9]{12})\.xml/
 
-const inFileName = "DOI_Mismatch.csv";
-
-const testObjectsInArray = (a, s) => {
-    for (const wp of a) {
-        if (wp.identifier === s) {
-            wp.duplicate = 'true'
-            return true;
+const extractInfo = (arr) => {
+    const id = regexId.exec(arr[0])
+    if (!id || id.length < 2) return
+    const url = arr[1]
+    return [`10.1787/${id[1]}`, url]
+}
+const regexLine = /^[^-]+\.xml$/
+const readParser = parse({columns: false}, function(err, lines) {
+    let res = [];
+    for (const line of lines) {
+        const fileName = line[0]
+        if (fileName.match(regexLine)) {
+            const record = extractInfo(line)
+            if (record) res.push(record)
         }
     }
-    return false;
-}
-
-const regex = /^[^-]+$/;
-
-const readParser = parse({columns: false}, function(err, line) {
-    let res = [];
-    const item = line[0]
-    if (item.match(regex)) {
-        res.push(line)
-    }
-    stringify(wps, {
-        headers: true
-    }, function(err, output) {
-        fs.writeFile(__dirname + '/final.csv', output)
-    })
-    
+    fs.writeFileSync(__dirname + '/test.json', JSON.stringify(res))
+    stringify(res, function(err, output) {
+        if (err) {
+            console.log(err)
+            return
+        }
+        fs.writeFileSync(__dirname + '/final.csv', output)
+        }
+    );
 });
-
-fs.createReadStream(__dirname + '\\' + inFileName).pipe(readParser);
-
-// const wp = fs.readFileSync('./workingpaperids.txt').toString().replace(/\r\n/g, '\n').split('\n');
-
-
-
-// wp.forEach(wpid => {
-//     if (sl.includes(wpid)) {
-//         if (res.includes (wpid)) {
-//             console.log(`${wpid} - duplicate`)
-//         } else {
-//             res.push(wpid);
-//             fs.writeFileSync('out.csv', `https://doi.org/10.1787/${wpid}\n`, {
-//                 flag: "a+"
-//             })
-//         }
-//     }
-// });
-// console.log(res.length)
+fs.createReadStream(__dirname + '/' + inFileName).pipe(readParser);
